0049d4d0d7f5aa3403429d9b121116538aaa0763
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-11-07 07:01:27 +0700

    Update info-setmode.js

--- plugins/info-setmode.js
+++ plugins/info-setmode.js
@@ -2,7 +2,7 @@ function handler(m) {
   m.reply('Mau matikan?\nMau hidupkan?')
   this.send2ButtonLoc(m.chat, "https://telegra.ph/file/3324243c9673f484c5930.jpg", `╭─〔  SET MODE  〕─⬡
   
-⊙ Mode : ${global.opts['self'] ? 'Self' : 'Publik'}`, 'Click Here', 'BOT PUBLIC', '.public', 'BOT SELF', '.self')
+⊙ Mode : ${global.opts['Publik'] ? 'Self' : 'Publik'}`, 'Click Here', 'BOT PUBLIC', '.public', 'BOT SELF', '.self')
 }
 handler.help = [ 'setmode']
 handler.tags = ['main']
